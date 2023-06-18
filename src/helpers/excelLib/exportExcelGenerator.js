a1acfcf791455c02d783f8fd5eed188caeca9cb8
emirr <emir.haikal@kalbe.co.id>
2023-06-18 17:28:08 +0700

    update export default

--- src/helpers/excelLib/exportExcelGenerator.js
+++ src/helpers/excelLib/exportExcelGenerator.js
@@ -1,6 +1,6 @@
 import { utils, writeFile } from "xlsx";
 
-export function generateExcel(data, fileName) {
+export default function generateExcel(data, fileName) {
   const worksheet = utils.json_to_sheet(data);
   const workbook = utils.book_new();
   utils.book_append_sheet(workbook, worksheet, "Sheet 1");
